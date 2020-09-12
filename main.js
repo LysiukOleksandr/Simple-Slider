0911dd212dc8537379045eda53ba7d8685447ce7
unknown <olysiuk01@gmail.com>
2020-09-12 14:47:08 +0300

    slider using recursive setTimeout

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-document.querySelector("button").onclick = mySlider;
-
 let sliderImages = document.querySelectorAll(".slider img");
 let count = 0;
+let timer;
 
 function mySlider() {
-  if (count < sliderImages.length) {
-    let img = sliderImages[count];
-    img.style.display = "block";
-    img.classList.add("animate__animated", "animate__fadeInTopRight");
-    img.addEventListener("animationend", () => {
-      img.classList.add(
-        "animate__animated",
-        "animate__fadeOutTopLeft",
-        "animate__delay-2s"
-      );
-    });
-    count++;
-  } else {
-    startNewSlider();
-  }
+  timerId = setTimeout(function slider() {
+    if (count < sliderImages.length) {
+      let img = sliderImages[count];
+      img.style.display = "block";
+      img.classList.add("animate__animated", "animate__fadeInTopRight");
+      img.addEventListener("animationend", () => {
+        img.classList.add("animate__animated", "animate__fadeOutTopLeft");
+      });
+      count++;
+    } else {
+      sliderImages.forEach((element) => {
+        element.classList.remove(
+          "animate__animated",
+          "animate__fadeOutTopLeft",
+          "animate__fadeInTopRight"
+        );
+        element.style.display = "none";
+      });
+      count = 0;
+    }
+    console.log(count);
+    timerId = setTimeout(slider, 4000);
+  }, 1000);
 }
 
-function startNewSlider() {
-  sliderImages.forEach((element) => {
-    element.classList.remove(
-      "animate__animated",
-      "animate__fadeOutTopLeft",
-      "animate__fadeInTopRight"
-    );
-    element.style.display = "none";
-  });
-  count = 0;
-  mySlider();
-}
+mySlider();
